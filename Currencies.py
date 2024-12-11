0c574bd7d88e54b26b4cc8d0443d74df6833806a
cherta285 <cherta285@gmail.com>
2024-12-11 21:54:02 +0500

    Добавление файла Currencies.py

--- Currencies.py
+++ Currencies.py
@@ -1,10 +1,13 @@
+import requests
+import xmltodict
 
+url = "http://www.cbr.ru/scripts/XML_val.asp"
+response = requests.get(url)
+data = xmltodict.parse(response.content)
+print(data)
 
-
-def main():
-	
-	return 0
-
-if __name__ == '__main__':
-	main()
-
+my_array = []
+for item in data['Valuta']['Item']:
+    my_set=[item['EngName'],item['Name'], item['Nominal']]
+    my_array.append(my_set)
+    print(my_set)
